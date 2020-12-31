83548f688a657de3ac78a1c9ed3154d4da8a060c
bullyb0yBristol <proudofbull@hotmail.com>
2020-12-31 16:48:34 +0000

    Up to 4-2
    Starting loops in python

---
+++ chapter_04/TIY.py
@@ -0,0 +1,16 @@
+#4-1 - pizzas
+print("4-1")
+pizzas = ["Pepperoni","Margherita","Ham and Cheese"]
+#Now print the name of each pizza
+for name in pizzas:
+	print(f"I like {name} pizza")
+print("I really love pizza\n\n")
+
+#4-2 animals
+print("4-2")
+animals = ["Zebra","Lion","Elephant","Koala","Monkey"]
+#Now print each animal
+for animal in animals:
+	print (f"A {animal} would make a great pet")
+print("In fact, any of the above animals would make a great pet!\n\n")
+
