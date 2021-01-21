526b53c58a7adddedbddb7da3919f20acc73c680
bullyb0yBrizzle <67582578+bullyb0yBrizzle@users.noreply.github.com>
2021-01-21 22:51:04 +0000

    Up to classes

--- chapter_08/TIY.py
+++ chapter_08/TIY.py
@@ -252,3 +252,44 @@ def car_info(manufacturer,model,**specifications):
 car_details = car_info("ford","satsuma",color="orange",wheels="4",doors=5)
 print(car_details)
 
+
+# 
+
+# 8-15. Printing Models: Put the functions for the example printing_models.py 
+# in a separate file called printing_functions.py. Write an import statement
+# at the top of printing_models.py, and modify the file to use the imported
+# functions.
+
+import printing_functions as pf
+        
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
+
+# 8-16. Imports: Using a program you wrote that has one function 
+# in it, store that function in a separate file. Import the 
+# function into your main program file, and call the function 
+# using each of these approaches:
+
+#import module_name
+#from module_name import function_name
+#from module_name import function_name as fn
+#import module_name as mn
+#from module_name import *
+
+import pets 
+pets.describe_pet(pet_name='willie')
+
+from pets import describe_pet 
+describe_pet(pet_name='willie')
+
+from pets import describe_pet as dp
+dp(pet_name='willie')
+
+import pets as pp
+pp.describe_pet(pet_name='willie')
+
+from pizza import *
+make_pizza("Large","Ham","Sallami")
