5acab4f073ee0c971e60ea14bd0d69b835ddaa71
bullyb0yBrizzle <67582578+bullyb0yBrizzle@users.noreply.github.com>
2021-01-25 21:54:22 +0000

    Update TIY.py

--- chapter_09/TIY.py
+++ chapter_09/TIY.py
@@ -185,3 +185,21 @@ print(me.login_attempts)
 print("\n")
 
 
+#9-6. Ice Cream Stand: An ice cream stand is a specific kind of
+# restaurant. Write a class called IceCreamStand that inherits
+# from the Restaurant class you wrote in Exercise 9-1 (page 162)
+# or Exercise 9-4 (page 167). Either version of the class will
+# work; just pick the one you like better. Add an attribute called
+# flavors that stores a list of ice cream flavors. Write a method
+# that displays these flavors. Create an instance of IceCreamStand,
+# and call this method.
+
+class IceCreamStand(restaurant):
+
+	def __init__(self,restaurant_name,cuisine_type):
+		super().__init__(self,restaurant_name,cuisine_type)
+		self.flavors = ["Honey","Bumsuckle","Choccy chip"]
+
+	def display_flavors(self)
+		for 
+
